280ce23056f2132eb4841d420a7a969269b32bcf
Erlan <erlanchuacbaev@gmail.com>
2023-06-26 19:15:48 +0600

    Testing

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,16 +1,17 @@
 namespace SquareEquationLib;
+
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10,-9);
+        double eps = 1e-9;
         if (-eps < a && a < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
         {
             throw new ArgumentException();
         }
         b = b / a;
         c = c / a;
-        double d = Math.Pow(b,2) - 4 * c;
+        double d = b * b - 4 * c;
         double[] zeroRoots = new double[] { };
         if (d <= -eps) return zeroRoots;
         else if (-eps < d & d < eps)
@@ -26,5 +27,6 @@ public class SquareEquation
             twoRoots[1] = c / twoRoots[0];
             return twoRoots;
         }
+
     }
 }
