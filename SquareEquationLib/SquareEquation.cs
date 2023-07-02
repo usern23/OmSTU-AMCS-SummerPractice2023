09052abb355fe7473fa75cea2f7b211434cd72f9
Erlan <erlanchuacbaev@gmail.com>
2023-07-02 17:58:27 +0000

    try3

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -3,28 +3,46 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10,-9);
-        if (-eps < a && a < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        double eps = 1e-6;
+
+        if ((a > -eps && a < eps) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
         {
-            throw new ArgumentException();
+            throw new System.ArgumentException("Invalid Values");
         }
         b = b / a;
         c = c / a;
-        double d = Math.Pow(b,2) - 4 * c;
-        double[] zeroRoots = new double[] { };
-        if (d <= -eps) return zeroRoots;
+
+        double d = b * b - 4 * c;
+        double[] answerZero = new double[] { };
+        double[] answerOne = new double[1];
+        double[] answerTwo = new double[2];
+
+        if (d <= -eps)
+        {
+            return answerZero;
+        }
         else if (-eps < d & d < eps)
-        { 
-            double[] oneRoots = new double[1];
-            oneRoots[0] = -(b) / 2;
-            return oneRoots;
+        {
+            double x1 = -(b) / 2;
+            answerOne[0] = x1;
+            return answerOne;
+        }
+        else if (b < eps)
+        {
+            double x1 = -((b + Math.Sqrt(d)) / 2);
+            double x2 = c / x1;
+            answerTwo[0] = x1;
+            answerTwo[1] = x2;
+            return answerTwo;
         }
         else
         {
-            double[] twoRoots = new double[2];
-            twoRoots[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
-            twoRoots[1] = c / twoRoots[0];
-            return twoRoots;
+            double x1 = -((b + Math.Sign(b) * Math.Sqrt(d)) / 2);
+            double x2 = c / x1;
+            answerTwo[0] = x1;
+            answerTwo[1] = x2;
+            return answerTwo;
         }
+
     }
 }
